cd61bd1807fdde458615547b20688a57f234bcac
vikash <vikashraj144@gmail.com>
2019-02-18 23:37:58 +0530

    add reader compoent

---
+++ src/app/add-reader/add-reader.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Reader } from '../models/reader';
+import { BadgeService } from '../services/badge.service';
+
+@Component({
+  selector: 'app-add-reader',
+  templateUrl: './add-reader.component.html',
+  styles: []
+})
+export class AddReaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  saveReader(formValues: any): void {
+    let newReader: Reader = <Reader>formValues;
+    newReader.readerID = 0;
+    console.log(newReader);
+    console.warn('Save new reader not yet implemented.');
+  }
+
+}
