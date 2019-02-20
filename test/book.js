5a87b9e1ac273912ac038f6ac19740de4da14b37
vikash <vikashraj144@gmail.com>
2019-02-20 23:13:40 +0530

    unit test cases added for get books

---
+++ test/book.js
@@ -0,0 +1,32 @@
+/* 
+Unit test case for api
+*/
+
+process.env.NODE_ENV = 'test';
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Books', () => {
+    
+/*
+  * Test the /GET route
+  */
+  describe('/GET /api/books', () => {
+      it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/api/books')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                // res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
