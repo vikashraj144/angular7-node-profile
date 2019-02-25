44c47b07406da6c1eb601c981540301d43a25eb0
vikash <vikashraj144@gmail.com>
2019-02-25 18:56:22 +0530

    profiler

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-first-project!');
+    expect(page.getTitleText()).toEqual('Welcome to profiler!');
   });
 
   afterEach(async () => {
