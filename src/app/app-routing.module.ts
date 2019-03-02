85257ae1f4285fffde69e334fa81190e3c661f72
vikash <vikashraj144@gmail.com>
2019-03-02 13:05:38 +0530

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,22 @@ import { AddReaderComponent } from "../app/add-reader/add-reader.component";
 import { DashboardComponent } from "../app/dashboard/dashboard.component";
 import { EditBookComponent } from "../app/edit-book/edit-book.component";
 import { EditReaderComponent } from "../app/edit-reader/edit-reader.component";
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectComponent } from './components/project/project.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
+  
   { path: 'addreader', component: AddReaderComponent },
   { path: 'editreader/:id', component: EditReaderComponent },
   { path: 'editbook/:id', component: EditBookComponent },
