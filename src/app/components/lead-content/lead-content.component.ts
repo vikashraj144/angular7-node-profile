39b05096c2172b4f08c6b0443e9d86f0c11a365d
vikash <vikashraj144@gmail.com>
2019-03-30 12:57:44 +0530

    service added in lead com

--- src/app/components/lead-content/lead-content.component.ts
+++ src/app/components/lead-content/lead-content.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import {ProfilerService} from './../../services/profile.service';
+
 @Component({
   selector: 'app-lead-content',
   templateUrl: './lead-content.component.html',
-  styleUrls: ['./lead-content.component.scss']
+  styleUrls: ['./lead-content.component.scss'],
+  providers: [ProfilerService]
 })
 export class LeadContentComponent implements OnInit {
 
-  constructor() { }
+  leadData:any;
+  constructor(private profilerService: ProfilerService) { }
 
   ngOnInit() {
+    this.profilerService.getEducation()
+    .subscribe(d=>{
+      console.log("data",d);
+      this.leadData = d
+    });
   }
 
-}
+} 
