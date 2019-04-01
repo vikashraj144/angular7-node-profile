d8f4a013e87377b118c0187c28c5f2fcfaf043b5
vikash <vikashraj144@gmail.com>
2019-04-01 12:02:13 +0530

    about us file

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
+  providers: [ProfilerService]
 })
 export class AboutUsComponent implements OnInit {
 
-  constructor() { }
+  leadData: any;
+  constructor(private profilerService: ProfilerService) { }
 
   ngOnInit() {
+    this.profilerService.getLead()
+    .subscribe(d=>{
+      this.leadData = d
+    });
   }
 
 }
