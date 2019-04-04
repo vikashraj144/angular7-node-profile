c55de2726468eb43c726eebb2536d555b9a874c9
vikash <vikashraj144@gmail.com>
2019-04-04 15:25:13 +0530

    serice updated

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -6,6 +6,8 @@ import { catchError } from 'rxjs/operators';
 import { environment } from './../../environments/environment';
 import { Education } from '../models/education';
 import { ProfileError } from '../models/ProfileError';
+import { Lead } from '../models/lead';
+import { Experience } from '../models/experience';
 
 @Injectable()
 
@@ -13,18 +15,18 @@ export class ProfilerService {
     baseUrl = environment.baseUrlLocal;
     constructor(private http: HttpClient) { }
 
-    getEducation(): Observable<Education | ProfileError> {
-        return this.http.get<Education>(this.baseUrl + '/assets/json/education.json')
-          .pipe(
-            catchError(this.handleError)
-          );
-      }
+  getEducation(): Observable<Education | ProfileError> {
+    return this.http.get<Education>(this.baseUrl + '/assets/json/education.json')
+      .pipe(
+        catchError(this.handleError)
+    );
+  }
 
-      private handleError(error: HttpErrorResponse): Observable<ProfileError> {
-        const dataError = new ProfileError();
-        dataError.errorNumber = 100;
-        dataError.message = error.statusText;
-        dataError.friendlyMessage = 'An error occurred retrieving data.';
-        return throwError(dataError);
-      }
+  private handleError(error: HttpErrorResponse): Observable<ProfileError> {
+    const dataError = new ProfileError();
+    dataError.errorNumber = 100;
+    dataError.message = error.statusText;
+    dataError.friendlyMessage = 'An error occurred retrieving data.';
+    return throwError(dataError);
+  }
 }
