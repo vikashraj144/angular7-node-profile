e580fe36458c793298bc13cc0bfcc023fcfc573c
vikash <vikashraj144@gmail.com>
2019-04-04 15:22:10 +0530

    lead class

---
+++ src/app/models/lead.ts
@@ -0,0 +1,13 @@
+export class Lead {
+    userId: Number;
+    firstName: String;
+    middleName: String;
+    lastName: String;
+    headline: String;
+    aboutMe: String;
+    totalExp: Number;
+    anualSalary: String;
+    currentLocation: String;
+    mobileNo: Number;
+    email: String;
+}
