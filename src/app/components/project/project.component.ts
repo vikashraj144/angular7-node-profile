51a8a369521a89ec6ca9d46232e6e7ccabb169f1
vikash <vikashraj144@gmail.com>
2019-04-05 19:02:55 +0530

    project comp added

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
+  providers: [ProfilerService]
+
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  projects: any;
+  constructor(private profilerService: ProfilerService) { }
 
   ngOnInit() {
+    this.profilerService.getProject().subscribe( data => {
+      this.projects = data;
+    });
   }
 
 }
