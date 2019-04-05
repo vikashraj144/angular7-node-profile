07346f08a56ff7889a5f18226f1d0699afcffc77
vikash <vikashraj144@gmail.com>
2019-04-05 11:43:19 +0530

    education service integrated

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
+  providers:[ProfilerService]
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  education: any;
+  constructor(private profilerService: ProfilerService) { }
 
   ngOnInit() {
+    this.profilerService.getEducation()
+    .subscribe(d=>{
+      this.education = d
+    });
   }
 
 }
