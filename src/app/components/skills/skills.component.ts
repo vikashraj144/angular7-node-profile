d603b651ca57ea738d9f708da2308e7896e68fff
vikash <vikashraj144@gmail.com>
2019-04-05 19:02:14 +0530

    skill service imp

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfilerService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
+  providers:[ProfilerService]
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  skills: any;
+  constructor(private profilerService: ProfilerService) { }
 
   ngOnInit() {
+    this.profilerService.getSkills().subscribe(data => {
+      this.skills = data;
+    });
   }
 
 }
