78580cbba04a569d20e0bf02eea0faf8323ce7f8
vikash <vikashraj144@gmail.com>
2019-07-06 15:21:57 +0530

    Country component added

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfilerService} from './../services/profile.service';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
-  styleUrls: ['./country.component.scss']
+  styleUrls: ['./country.component.scss'],
+  providers: [ProfilerService]
+  
 })
 export class CountryComponent implements OnInit {
 
-  constructor() { }
+  country:any;
+  constructor(private profilerService: ProfilerService) { }
 
   ngOnInit() {
+    this.profilerService.getCountry()
+    .subscribe(d=>{
+      console.log("data",d);
+      this.country = d
+    });    
   }
 
 }
